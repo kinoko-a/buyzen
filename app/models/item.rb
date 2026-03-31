fb0f2779951bc8df36b8b02578f748d8062908d0
kinoko-a <kmk.aok.3x@gmail.com>
2026-03-31 18:16:35 +0900

    change: アイテム用メモのカラム名を変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
-  validates :thinking_note, length: { maximum: 65_535 }
+  validates :note, length: { maximum: 65_535 }
 
   enum :status, { thinking: 0, decided_buy: 1, decided_skip: 2 }
   enum :cooldown_duration, { minutes_30: 0, hours_24: 1, days_3: 2 }
