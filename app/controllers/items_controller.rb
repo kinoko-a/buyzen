2e10a96c571d6c9bd7816c2be5d284f7fb643063
kinoko-a <kmk.aok.3x@gmail.com>
2026-04-02 14:41:35 +0900

    fix: lint指摘箇所を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,22 +24,22 @@ class ItemsController < ApplicationController
     @item = current_user.items.build(item_params)
     choice = params[:cooldown_choice]
 
-    if choice == 'skip'
+    if choice == "skip"
       @item.skip_cooldown!
     elsif choice.present? && Item.cooldown_durations.key?(choice)
       @item.cooldown_duration = choice
     end
 
     if @item.save
-      if choice == 'skip'
+      if choice == "skip"
         # ! 後で変更(クールダウンスキップ時は購入判断画面に遷移) !
-        redirect_to item_path(@item), flash: { success: t('flash.items.create.success') }
+        redirect_to item_path(@item), flash: { success: t("flash.items.create.success") }
       else
         # クールダウンタイマー使用時はアイテム詳細画面に遷移
-        redirect_to item_path(@item), flash: { success: t('flash.items.create.success') }
+        redirect_to item_path(@item), flash: { success: t("flash.items.create.success") }
       end
     else
-      flash.now[:alert] = t('flash.items.create.failure')
+      flash.now[:alert] = t("flash.items.create.failure")
       render :new, status: :unprocessable_entity
     end
   end
