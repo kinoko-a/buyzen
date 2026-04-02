be23e341c2035ec2618fa8547b7d490197658426
kinoko-a <kmk.aok.3x@gmail.com>
2026-04-03 00:00:30 +0900

    add: アイテム削除機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "static_pages#top"
   resource :dashboards, only: [ :show ]
-  resources :items, only: %i[index show new create edit update]
+  resources :items, only: %i[index show new create edit update destroy]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
